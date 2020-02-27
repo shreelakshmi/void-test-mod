fd104316e72134fade267fcec77f8cc53395a0f1
shreelakshmi <shree.gm@gmail.com>
2020-02-27 17:23:40 +0530

    First Commit

---
+++ src/test/java/org/dbpedia/databus/void_test_mod/AppTest.java
@@ -0,0 +1,20 @@
+package org.dbpedia.databus.void_test_mod;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
